bb822e00478753fd7cdef7ae164ca6a994a2fb9c
Marlon <marlon.da.silva996@gmail.com>
2026-04-14 19:51:30 -0300

    introdução a estrutura condicional

---
+++ Desenvolvimento de Software/Aula 02 - Estruturas Lógicas e Resolução de Problemas com JavaScript/script.js
@@ -0,0 +1,25 @@
+// estruturas condicionais
+// == (igual a)
+// === (estritamente igual a)
+// >, < (maior que, menor que)
+// >=, <= (maior ou igual, menor ou igual)
+// != (diferente de)
+// !== (estritamente diferente de)
+
+console.log(`"1" == 1: ${"1" == 1}`);
+console.log(`"1" === 1: ${"1" === 1}`);
+console.log(`20 > 20: ${20 > 20}`);
+console.log(`20 <= 20: ${20 <= 20}`);
+console.log(`1 != 2: ${1 != 2}`);
+console.log(`1 !== "1": ${1 !== "1"}`);
+console.log("");
+
+let idade = 18;
+
+if (idade >= 18) {
+    console.log("É maior de idade!");
+} else {
+    console.log("É menor de idade!");
+}
+
+
