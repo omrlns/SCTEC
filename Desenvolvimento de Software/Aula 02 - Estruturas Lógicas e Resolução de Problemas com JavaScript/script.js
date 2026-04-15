958e1366c6c536bc6a2f5fcdf7e364435c7402bb
Marlon <marlon.da.silva996@gmail.com>
2026-04-14 21:06:48 -0300

    explorando e manipulando listas

--- Desenvolvimento de Software/Aula 02 - Estruturas Lógicas e Resolução de Problemas com JavaScript/script.js
+++ Desenvolvimento de Software/Aula 02 - Estruturas Lógicas e Resolução de Problemas com JavaScript/script.js
@@ -52,3 +52,28 @@ for (let i = 0; i < 10; i++) {
     console.log(i + 1);
 }
 
+let frutas = ["Maracujá", "Maçã", "Uva", "Abacaxi", "Laranja"]
+
+console.log(frutas) // imprime todas as frutas no console
+console.log(frutas[0]) // Maracujá
+
+console.log("")
+
+// uma forma de percorrer todos os itens de uma lista
+for (i in frutas) {
+    console.log(frutas[i])
+}
+
+console.log("")
+
+console.log(`Tamanho da "Lista" de Frutas: ${frutas.length}`) // informa o tamanho da  lista
+
+console.log("")
+
+frutas.push("Banana") // adicionando uma nova fruta na lista (por padrão, o novo item é adicionado ao final da lista)
+console.log(frutas)
+
+console.log("")
+
+frutas.pop() // remove um valor da lista (por padrão, remove o último item adicionado (pilha))
+console.log(frutas)
