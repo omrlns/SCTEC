8e7dbaa006ebe4b32a0ee675a1230c50c74eaac5
Marlon <marlon.da.silva996@gmail.com>
2026-05-01 00:34:23 -0300

    introdução ao javascript 1/2

---
+++ Primeiros Passos/Introdução à Programação Frontend e Backend/Backend/Aula 17/script.js
@@ -0,0 +1,35 @@
+function saudacao() {
+    return console.log("Hello World!");
+}
+
+saudacao();
+
+function somar(valorX, valorY) {
+    const resultado = valorX + valorY;
+    return console.log(resultado);
+}
+
+somar(1, 1);
+somar(1, 2);
+somar(2, 3);
+somar(3, 4);
+somar(4, 5);
+
+function subtrair(valorX, valorY) {
+    const resultado = valorX - valorY;
+    return console.log(resultado);
+}
+
+function dividr(valorX, valorY) {
+    const resultado = valorX / valorY;
+    return console.log(resultado);
+}
+
+function multiplicar(valorX, valorY) {
+    const resultado = valorX * valorY;
+    return console.log(resultado);
+}
+
+subtrair(10, 10);
+dividr(33, 3);
+multiplicar(5, 25);
