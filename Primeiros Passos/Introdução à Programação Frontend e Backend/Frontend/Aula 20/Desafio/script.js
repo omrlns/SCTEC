4b527d061d3feb5eeb502f285a20d475b5d5878c
Marlon <marlon.da.silva996@gmail.com>
2026-05-03 00:23:36 -0300

    resolução do desafio da aula 20 + requisitos.txt

---
+++ Primeiros Passos/Introdução à Programação Frontend e Backend/Frontend/Aula 20/Desafio/script.js
@@ -0,0 +1,14 @@
+// alert("arquivo .html utilizado apenas para visualizar os testes no console.");
+
+let frutas = ["Araça", "Banana", "Carambola", "Damasco", "Laranja"];
+
+console.log(frutas); // exibe a lista "frutas" conforme a definição inicial
+console.log(frutas[2]); // exibe o terceiro item na lista, que nesse caso seria "Carambola"
+
+frutas[1] = "Maracujá"; // alterando o segundo item da lista de "Banana" para "Maracujá"
+
+console.log(frutas); // exibindo a lista após a alteração
+
+frutas.push("Uva", "Pêra"); // adiciona duas novas frutas ao final da lista
+
+console.log(frutas); // exibindo a lista após a alteração
