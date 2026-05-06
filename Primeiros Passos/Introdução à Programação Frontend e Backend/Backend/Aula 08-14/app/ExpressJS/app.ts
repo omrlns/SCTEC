1816e9ed8f698156d9fe37ca63601b6eb0d42fa4
Marlon <marlon.da.silva996@gmail.com>
2026-05-06 17:46:50 -0300

    atualizando o app.ts com o novo arquivo de routes

--- Primeiros Passos/Introdução à Programação Frontend e Backend/Backend/Aula 08-14/app/ExpressJS/app.ts
+++ Primeiros Passos/Introdução à Programação Frontend e Backend/Backend/Aula 08-14/app/ExpressJS/app.ts
@@ -1,12 +1,13 @@
 import express  from "express";
 import clientesRouter from "./routes/clients.js";
+import usersRouter from "./routes/users.js";
 import db from "./db.js"
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(clientesRouter);
+app.use(clientesRouter, usersRouter);
 app.set("view engine", "pug");
 app.set("views", "./views");
 
