12dbda0c5e13b91fce812a7741a1c0c8ce4ea897
Marlon <marlon.da.silva996@gmail.com>
2026-05-06 17:16:15 -0300

    criando users e usersModels

---
+++ Primeiros Passos/Introdução à Programação Frontend e Backend/Backend/Aula 08-14/app/ExpressJS/model/usersModel.ts
@@ -0,0 +1,22 @@
+import db from "../db.js";
+import sequelize from "sequelize";
+
+export default db.define("user", {
+    id: {
+        type: sequelize.INTEGER.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+    },
+    user: {
+        type: sequelize.STRING,
+        allowNull: false
+    },
+    password: {
+        type: sequelize.STRING,
+        allowNull: false
+    }
+}, {
+    timestamps: false,
+    freezeTableName: true
+});
