f0fcac07f600fa0842a11af45a4b42bead909925
Marlon <marlon.da.silva996@gmail.com>
2026-05-06 17:17:38 -0300

    adicionado um "clients" na frente da "/" das rotas

--- Primeiros Passos/Introdução à Programação Frontend e Backend/Backend/Aula 08-14/app/ExpressJS/routes/clients.ts
+++ Primeiros Passos/Introdução à Programação Frontend e Backend/Backend/Aula 08-14/app/ExpressJS/routes/clients.ts
@@ -3,16 +3,16 @@ import clientsController from "../controller/clientsController.js";
 
 const router = Router();
 
-router.get("/", clientsController.index);
+router.get("clients/", clientsController.index);
 
-router.get("/create", clientsController.create);
-router.post("/create", clientsController.store);
+router.get("clients/create", clientsController.create);
+router.post("clients/create", clientsController.store);
 
-router.get("/:id", clientsController.show);
+router.get("clients/:id", clientsController.show);
 
-router.get("/edit/:id", clientsController.edit);
-router.post("/edit/:id", clientsController.update);
+router.get("clients/edit/:id", clientsController.edit);
+router.post("clients/edit/:id", clientsController.update);
 
-router.get("/del/:id", clientsController.del);
+router.get("clients/del/:id", clientsController.del);
 
 export default router;
